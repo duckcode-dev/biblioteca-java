8883c655048f54bf33437504e55b54b143a7d990
duckcode-dev <patricio2395@hotmail.com>
2025-07-13 22:49:06 -0400

    content:actualizar comentario principal de archivo

--- main.java
+++ main.java
@@ -1,4 +1,10 @@
-
+/**
+ * Nombre del archivo: main.java
+ * Autor: duckcode-dev
+ * Fecha de creación: 13/07/2025
+ * Descripción: Punto de entrada principal para la aplicación de biblioteca.
+ * Licencia: MIT
+ */
 public class main {
     public static void main(String[] args) {
 
