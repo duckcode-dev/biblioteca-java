1d17252a7dcd630127918da536b34c6ca5ba797f
duckcode-dev <patricio2395@hotmail.com>
2025-07-16 23:48:38 -0400

    feat: agregar getters y setters en la clase Libro.java

--- libro.java
+++ libro.java
@@ -5,4 +5,37 @@ public class Libro {
     private String autor;
     private int anioPublicacion;
 
+    // getter and seteer
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
 }
