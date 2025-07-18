9ca5b1b987b7ef9df4ff873904f9660c10b5cfed
duckcode-dev <patricio2395@hotmail.com>
2025-07-17 23:29:59 -0400

    feat: agregar getter en la clase Biblioteca

--- Biblioteca.java
+++ Biblioteca.java
@@ -2,5 +2,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Biblioteca {
+
     private Set<Libro> libros = new HashSet<>();
+
+    public Set<Libro> getLibros() {
+        return libros;
+    }
+
 }
