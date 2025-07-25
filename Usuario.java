37e34333b7c2c5e5b96545743a1f291283cb51eb
duckcode-dev <patricio2395@hotmail.com>
2025-07-24 23:17:00 -0400

    feat: agregar getters and setters  a clase Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -3,4 +3,28 @@ public class Usuario {
     private String name;
     private String foneNumber;
 
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoneNumber() {
+        return foneNumber;
+    }
+
+    public void setFoneNumber(String foneNumber) {
+        this.foneNumber = foneNumber;
+    }
+
 }
